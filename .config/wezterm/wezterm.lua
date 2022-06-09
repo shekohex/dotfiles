0ae28cfb4d2daef2decd61f8c3982835c60b9435
Shady Khalifa <shekohex@gmail.com>
2022-06-09 13:58:01 +0200

    local echo

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -63,7 +63,7 @@ M.ssh_domains = {
     {
         name = "workstation",
         remote_address = "workstation",
-        local_echo_threshold_ms = 8,
+        local_echo_threshold_ms = 18,
     },
 }
 return M;
