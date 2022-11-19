3406eec6f330a5cf9ec01f4e7200faa4b2fb30d3
Shady Khalifa <dev+github@shadykhalifa.me>
2022-11-19 16:59:25 +0200

    updates

--- .config/lvim/lua/wezterm_helper.lua
+++ .config/lvim/lua/wezterm_helper.lua
@@ -1,14 +1,18 @@
 local M = {}
--- Read the current pane ID from the environment: $WEZTERM_PANE
--- and store it so we can filter by it later.
--- we also need to parse it as a number, since it is a string, fallback to 0
-local pane_id = tonumber(os.getenv 'WEZTERM_PANE') or 0
 
 -- Now, we need to execute a command to get all the active panes, filter it
 -- by the current pane ID, and then read it's title.
 -- The return value of this command is a json list of panes, so we need to
 -- parse it as json.
 local function get_current_pane_title()
+  -- Read the current pane ID from the environment: $WEZTERM_PANE
+  -- and store it so we can filter by it later.
+  -- we also need to parse it as a number, since it is a string, fallback to -1
+  local pane_id = tonumber(os.getenv 'WEZTERM_PANE') or -1
+  -- Check if it is -1 and if so, that means we are not running inside a wezterm env.
+  if pane_id == -1 then
+    return ''
+  end
   -- exec the command "wezterm cli list --format json"
   -- using vim api to exec commands.
   local output = function()
--- .zshrc
+++ .zshrc
@@ -46,3 +46,5 @@ alias hexdump=hx
 alias htop=btm
 alias du=dust
 alias http=xt
+
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
