86edae412d134946b584f7b1f1ea32dc164fa23b
Shady Khalifa <dev+github@shadykhalifa.me>
2023-04-15 03:25:59 +0200

    Updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,47 +1,65 @@
 local wezterm = require("wezterm")
-local M = {}
 
-M.font = wezterm.font_with_fallback({
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.font = wezterm.font_with_fallback({
 	"JetBrainsMono Nerd Font Mono",
 })
-M.font_size = 16
-M.default_cursor_style = "BlinkingUnderline"
-M.audible_bell = "Disabled"
-M.enable_tab_bar = true
-M.use_fancy_tab_bar = false
-M.hide_tab_bar_if_only_one_tab = true
-M.tab_max_width = 32
-M.window_decorations = "RESIZE"
-M.window_close_confirmation = "NeverPrompt"
-M.use_resize_increments = true
-M.window_background_opacity = 0.9
-M.initial_rows = 42
-M.initial_cols = 148
-M.window_padding = {
+config.font_size = 16
+config.default_cursor_style = "BlinkingUnderline"
+config.audible_bell = "Disabled"
+config.enable_tab_bar = true
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.tab_max_width = 32
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.use_resize_increments = true
+config.window_background_opacity = 0.9
+config.initial_rows = 42
+config.initial_cols = 148
+config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
 	bottom = 0,
 }
-M.use_ime = false
-M.enable_csi_u_key_encoding = true
+config.use_ime = false
+config.enable_wayland = true
+config.enable_csi_u_key_encoding = true
 
 local function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
-		return "Catppuccin Mocha"
+		return "Catppuccin configocha"
 	else
 		return "Catppuccin Latte"
 	end
 end
 
 local brighteness = "Light"
-M.color_scheme = scheme_for_appearance(brighteness)
+config.color_scheme = scheme_for_appearance(brighteness)
 
-M.ssh_domains = {
+config.ssh_domains = {
 	{
 		name = "workstation",
 		remote_address = "workstation",
 		local_echo_threshold_ms = 80,
 	},
 }
-return M
+
+config.unix_domains = {
+	{
+		name = "unix",
+	},
+}
+
+config.default_gui_startup_args = { "connect", "unix" }
+
+return config
