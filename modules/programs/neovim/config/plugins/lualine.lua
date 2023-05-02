ed7bbe5f6625a00ba2b36f03ae54e010df791ac5
Shady Khalifa <dev+github@shadykhalifa.me>
2023-05-02 21:08:36 +0300

    Update lualine make it pretty and simple

--- modules/programs/neovim/config/plugins/lualine.lua
+++ modules/programs/neovim/config/plugins/lualine.lua
@@ -13,62 +13,54 @@ local icons = {
 }
 
 require("lualine").setup({
-  {
-    options = {
-      theme = "auto",
-      globalstatus = true,
-      component_separators = {
-        left = "│",
-        right = "│",
+  options = {
+    theme = "auto",
+    globalstatus = true,
+    component_separators = {},
+    section_separators = {},
+  },
+  sections = {
+    lualine_a = { "mode" },
+    lualine_b = { "branch" },
+    lualine_c = {
+      {
+        "diagnostics",
+        symbols = {
+          error = icons.diagnostics.Error,
+          warn = icons.diagnostics.Warn,
+          info = icons.diagnostics.Info,
+          hint = icons.diagnostics.Hint,
+        },
       },
-      section_separators = {
-        left = "│",
-        right = "│",
+      {
+        "filetype",
+        icon_only = true,
+        separator = "",
+        padding = {
+          left = 1,
+          right = 0,
+        },
+      },
+      { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
+    },
+    lualine_x = {
+      {
+        "diff",
+        symbols = {
+          added = icons.git.added,
+          modified = icons.git.modified,
+          removed = icons.git.removed,
+        },
       },
     },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch" },
-      lualine_c = {
-        {
-          "diagnostics",
-          symbols = {
-            error = icons.diagnostics.Error,
-            warn = icons.diagnostics.Warn,
-            info = icons.diagnostics.Info,
-            hint = icons.diagnostics.Hint,
-          },
-        },
-        {
-          "filetype",
-          icon_only = true,
-          separator = "",
-          padding = {
-            left = 1,
-            right = 0,
-          },
-        },
-        { "filename", path = 1, symbols = { modified = "  ", readonly = "", unnamed = "" } },
-      },
-      lualine_x = {
-        {
-          "diff",
-          symbols = {
-            added = icons.git.added,
-            modified = icons.git.modified,
-            removed = icons.git.removed,
-          },
-        },
-      },
-      lualine_y = {
-        { "progress", separator = " ",                  padding = { left = 1, right = 0 } },
-        { "location", padding = { left = 0, right = 1 } },
-      },
-      lualine_z = {
-        function()
-          return " " .. os.date("%R")
-        end,
-      },
+    lualine_y = {
+      { "progress", separator = " ",                  padding = { left = 1, right = 0 } },
+      { "location", padding = { left = 0, right = 1 } },
+    },
+    lualine_z = {
+      function()
+        return " " .. os.date("%R")
+      end,
     },
   },
 })
