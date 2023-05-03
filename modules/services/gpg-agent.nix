082404e68caa330a932c02d8d5e1aa94bb2fe1e3
Shady Khalifa <dev+github@shadykhalifa.me>
2023-05-03 12:52:41 +0300

    stop using kwallet

--- modules/services/gpg-agent.nix
+++ modules/services/gpg-agent.nix
@@ -1,5 +1,3 @@
-{ pkgs, ... }:
-
 {
   services.gpg-agent = {
     enable = true;
@@ -9,9 +7,7 @@
     defaultCacheTtlSsh = 60480000;
     maxCacheTtl = 60480000;
     sshKeys = [ "A61DAE8099822438C17ADB1AEF4439BF5AAFC822" ];
-    pinentryFlavor = "tty";
     extraConfig = ''
-      pinentry-program ${pkgs.kwalletcli}/bin/pinentry-kwallet
       allow-loopback-pinentry
     '';
   };
