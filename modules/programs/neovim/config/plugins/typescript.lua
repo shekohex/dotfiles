237253cf36958c8eeaac061cc38575cb28b65f26
Shady Khalifa <dev+github@shadykhalifa.me>
2023-05-05 10:52:07 +0300

    support for jsonc files for deno

--- modules/programs/neovim/config/plugins/typescript.lua
+++ modules/programs/neovim/config/plugins/typescript.lua
@@ -3,14 +3,14 @@ local typescript_loaded, typescript = pcall(require, "typescript")
 if typescript_loaded then
   typescript.setup({
     server = {
-      root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json"),
+      root_dir = lspconfig.util.root_pattern("package.json"),
     },
   })
 end
 
 -- Deno LSP
 lspconfig.denols.setup({
-  root_dir = lspconfig.util.root_pattern("deno.json"),
+  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
   init_options = {
     lint = true,
   },
