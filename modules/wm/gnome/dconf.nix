970dcfefa100bfaf751d49021b8eadc24ffbe1d5
Shady Khalifa <dev+github@shadykhalifa.me>
2023-05-09 18:57:30 +0300

    Update gnome settings

--- modules/wm/gnome/dconf.nix
+++ modules/wm/gnome/dconf.nix
@@ -5,41 +5,8 @@ with lib.hm.gvariant;
 
 {
   dconf.settings = {
-    "ca/desrt/dconf-editor" = {
-      saved-pathbar-path = "/apps/seahorse/windows/key-manager/";
-      saved-view = "/";
-      window-height = 500;
-      window-is-maximized = false;
-      window-width = 540;
-    };
-
-    "org/blueman/general" = {
-      plugin-list = [ "NetUsage" ];
-      window-properties = [ 568 350 710 509 ];
-    };
-
-    "org/blueman/plugins/autoconnect" = {
-      services = [ (mkTuple [ "/org/bluez/hci0/dev_88_D0_39_97_7F_9F" "00000000-0000-0000-0000-000000000000" ]) ];
-    };
-
-    "org/blueman/plugins/powermanager" = {
-      auto-power-on = "@mb true";
-    };
-
-    "org/blueman/plugins/recentconns" = {
-      recent-connections = "[{'adapter': '84:1B:77:0C:B0:C4', 'address': '88:D0:39:97:7F:9F', 'alias': 'Soundcore Flare 2', 'icon': 'audio-headset', 'name': 'Audio and input profiles', 'uuid': '00000000-0000-0000-0000-000000000000', 'time': '1683533105.9883206'}]";
-    };
-
-    "org/gnome/Console" = {
-      last-window-size = mkTuple [ 652 481 ];
-    };
-
     "org/gnome/GWeather4" = {
       temperature-unit = "centigrade";
-    };
-
-    "org/gnome/Weather" = {
-      locations = "[<(uint32 2, <('Cairo', 'HECA', true, [(0.52592587544986047, 0.54803338512621935)], [(0.52447144022429604, 0.54541539124822791)])>)>]";
     };
 
     "org/gnome/calculator" = {
@@ -123,7 +90,7 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/notifications" = {
-      application-children = [ "gnome-power-panel" "org-wezfurlong-wezterm" "spotify" "brave-browser" "org-telegram-desktop" "slack" ];
+      application-children = [ "gnome-power-panel" "org-wezfurlong-wezterm" "spotify" "brave-browser" "org-telegram-desktop" "slack" "org-gnome-evolution-alarm-notify" ];
     };
 
     "org/gnome/desktop/notifications/application/bitwarden" = {
@@ -235,7 +202,6 @@ with lib.hm.gvariant;
       notify-window-width = 766;
       notify-window-x = 610;
       notify-window-y = 342;
-      reminders-past = [ ];
     };
 
     "org/gnome/file-roller/listing" = {
@@ -306,7 +272,6 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/shell" = {
-      app-picker-layout = "[{'blueman-manager.desktop': <{'position': <0>}>, 'brave-browser.desktop': <{'position': <1>}>, 'org.gnome.Weather.desktop': <{'position': <2>}>, 'org.gnome.clocks.desktop': <{'position': <3>}>, 'org.gnome.Maps.desktop': <{'position': <4>}>, 'btop.desktop': <{'position': <5>}>, 'org.gnome.Photos.desktop': <{'position': <6>}>, 'org.gnome.Totem.desktop': <{'position': <7>}>, 'org.gnome.Calculator.desktop': <{'position': <8>}>, 'org.gnome.Calendar.desktop': <{'position': <9>}>, 'simple-scan.desktop': <{'position': <10>}>, 'org.gnome.Settings.desktop': <{'position': <11>}>, 'gnome-system-monitor.desktop': <{'position': <12>}>, 'ca.desrt.dconf-editor.desktop': <{'position': <13>}>, 'org.gnome.Extensions.desktop': <{'position': <14>}>, 'org.gnome.Nautilus.desktop': <{'position': <15>}>, 'cups.desktop': <{'position': <16>}>, 'nvim.desktop': <{'position': <17>}>, 'nixos-manual.desktop': <{'position': <18>}>, 'spotify.desktop': <{'position': <19>}>, 'org.gnome.font-viewer.desktop': <{'position': <20>}>, 'org.telegram.desktop.desktop': <{'position': <21>}>, 'org.gnome.TextEditor.desktop': <{'position': <22>}>, 'vlc.desktop': <{'position': <23>}>}, {'org.wezfurlong.wezterm.desktop': <{'position': <0>}>, 'xterm.desktop': <{'position': <1>}>, 'org.gnome.FileRoller.desktop': <{'position': <2>}>, 'org.gnome.Connections.desktop': <{'position': <3>}>, 'org.gnome.Console.desktop': <{'position': <4>}>, 'org.gnome.baobab.desktop': <{'position': <5>}>, 'org.gnome.DiskUtility.desktop': <{'position': <6>}>, 'org.gnome.Evince.desktop': <{'position': <7>}>, 'org.gnome.eog.desktop': <{'position': <8>}>, 'org.gnome.Logs.desktop': <{'position': <9>}>, 'org.gnome.seahorse.Application.desktop': <{'position': <10>}>, 'org.gnome.tweaks.desktop': <{'position': <11>}>}]";
       disable-user-extensions = false;
       disabled-extensions = [ "gsconnect@andyholmes.github.io" "space-bar@luchrioh" "Vitals@CoreCoding.com" "auto-move-windows@gnome-shell-extensions.gcampax.github.com" "apps-menu@gnome-shell-extensions.gcampax.github.com" "window-list@gnome-shell-extensions.gcampax.github.com" "user-theme@gnome-shell-extensions.gcampax.github.com" "Hide_Activities@shay.shayel.org" "places-menu@gnome-shell-extensions.gcampax.github.com" "workspace-indicator@gnome-shell-extensions.gcampax.github.com" ];
       enabled-extensions = [ "windowsNavigator@gnome-shell-extensions.gcampax.github.com" "drive-menu@gnome-shell-extensions.gcampax.github.com" "native-window-placement@gnome-shell-extensions.gcampax.github.com" "screenshot-window-sizer@gnome-shell-extensions.gcampax.github.com" "appindicatorsupport@rgcjonas.gmail.com" "search-light@icedman.github.com" "blur-my-shell@aunetx" "just-perfection-desktop@just-perfection" ];
@@ -347,8 +312,22 @@ with lib.hm.gvariant;
       compatibility = false;
     };
 
+    "org/gnome/shell/extensions/blur-my-shell/overview" = {
+      style-components = 3;
+    };
+
     "org/gnome/shell/extensions/blur-my-shell/panel" = {
-      customize = true;
+      blur = true;
+      brightness = 0.58;
+      color = mkTuple [ 0.0 0.0 ];
+      customize = false;
+      noise-lightness = 0.0;
+      override-background = true;
+      override-background-dynamically = true;
+      sigma = 113;
+      static-blur = true;
+      style-panel = 0;
+      unblur-in-overview = false;
     };
 
     "org/gnome/shell/extensions/blur-my-shell/window-list" = {
@@ -371,11 +350,12 @@ with lib.hm.gvariant;
     "org/gnome/shell/extensions/just-perfection" = {
       accessibility-menu = false;
       activities-button = false;
+      activities-button-icon-monochrome = true;
       animation = 3;
       app-menu = true;
       app-menu-icon = true;
       app-menu-label = false;
-      background-menu = true;
+      background-menu = false;
       clock-menu = true;
       clock-menu-position = 0;
       clock-menu-position-offset = 0;
@@ -392,22 +372,25 @@ with lib.hm.gvariant;
       panel-arrow = true;
       panel-corner-size = 0;
       panel-in-overview = true;
+      panel-notification-icon = true;
+      power-icon = false;
       quick-settings = true;
-      ripple-box = true;
+      ripple-box = false;
       search = false;
       show-apps-button = true;
       startup-status = 0;
       theme = false;
       top-panel-position = 0;
-      window-demands-attention-focus = false;
-      window-picker-icon = true;
+      weather = false;
+      window-demands-attention-focus = true;
+      window-picker-icon = false;
       window-preview-caption = true;
-      window-preview-close-button = true;
-      workspace = true;
+      window-preview-close-button = false;
+      workspace = false;
       workspace-background-corner-size = 0;
       workspace-popup = true;
       workspace-switcher-should-show = false;
-      workspaces-in-app-grid = true;
+      workspaces-in-app-grid = false;
       world-clock = false;
     };
 
@@ -477,7 +460,7 @@ with lib.hm.gvariant;
     };
 
     "org/gtk/gtk4/settings/color-chooser" = {
-      custom-colors = [ (mkTuple [ 0.0 0.0 ]) ];
+      custom-colors = [ (mkTuple [ 0.0 0.0 ]) (mkTuple [ 0.1411764770746231 0.1411764770746231 ]) (mkTuple [ 0.1411764770746231 0.1411764770746231 ]) (mkTuple [ 0.1411764770746231 0.1411764770746231 ]) (mkTuple [ 0.1411764770746231 0.1411764770746231 ]) (mkTuple [ 0.0 0.0 ]) ];
       selected-color = mkTuple [ true 0.0 ];
     };
 
