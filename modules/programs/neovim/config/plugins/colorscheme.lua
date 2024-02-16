4083ea8a55c5095abfe4f81e2c2ff9f6086b036f
Shady Khalifa <dev+github@shadykhalifa.me>
2024-02-16 17:25:14 +0200

    feat: add support to sync os theme

--- modules/programs/neovim/config/plugins/colorscheme.lua
+++ modules/programs/neovim/config/plugins/colorscheme.lua
@@ -4,8 +4,14 @@ local function config_colorscheme()
     return
   end
 
+  local os_is_dark = function()
+    local output = vim.fn.system(
+      "echo $(defaults read -globalDomain AppleInterfaceStyle &> /dev/null && echo 'dark' || echo 'light')"
+    )
+    return output:find("dark") ~= nil
+  end
   catppuccin.setup({
-    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    flavour = os_is_dark() and "mocha" or "latte",
     background = {
       light = "latte",
       dark = "mocha",
@@ -13,6 +19,30 @@ local function config_colorscheme()
     transparent_background = false,
     show_end_of_buffer = false, -- show the '~' characters after the end of buffers
   })
+
+  local set_os_theme = function()
+    if os_is_dark() then
+      vim.o.background = "dark"
+    else
+      vim.o.background = "light"
+    end
+  end
+
+  set_os_theme()
+
+  -- Register a command to toggle the theme
+  vim.api.nvim_create_user_command("ToggleTheme", function()
+    if vim.o.background == "dark" then
+      vim.o.background = "light"
+    else
+      vim.o.background = "dark"
+    end
+  end, { desc = "Toggle Neovim Theme", nargs = "*" })
+
+  -- Register a command to toggle the theme
+  vim.api.nvim_create_user_command("SyncTheme", function()
+    set_os_theme()
+  end, { desc = "Sync Neovim Theme with OS", nargs = "*" })
 
   -- setup must be called before loading
   vim.cmd.colorscheme("catppuccin")
