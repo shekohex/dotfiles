9c39b37bf9ae61b475a67b75bc04b1f891c681fe
Shady Khalifa <dev+github@shadykhalifa.me>
2024-03-11 09:51:06 +0200

    Update rust config

--- modules/programs/neovim/config/plugins/rust.lua
+++ modules/programs/neovim/config/plugins/rust.lua
@@ -6,16 +6,30 @@ vim.g.rustaceanvim = {
     default_settings = {
       -- rust-analyzer language server configuration
       ["rust-analyzer"] = {
-        lens = {
-          enable = true,
-        },
-        checkOnSave = {
+        diagnostics = { disabled = { "unresolved-proc-macro" } },
+        lens = { enable = true },
+        checkOnSave = true,
+        check = {
           command = "clippy",
           extraArgs = { "--tests" },
+          extraEnv = {
+            -- Skip WASM Build on substrate projects
+            SKIP_WASM_BUILD = 1,
+          },
+        },
+        cargo = {
+          buildScripts = { enable = true },
+          extraEnv = {
+            -- Skip WASM Build on substrate projects
+            SKIP_WASM_BUILD = 1,
+          },
         },
         inlayHints = { locationLinks = false },
         procMacro = {
           enable = true,
+          ignored = {
+            async_trait = { "async_trait" },
+          },
         },
         files = {
           excludeDirs = { "tests/node_modules", "node_modules", ".direnv" },
