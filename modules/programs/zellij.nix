18c57d585304ea2e08a6ec99b5151f9ec01b823f
Shady Khalifa <dev+github@shadykhalifa.me>
2024-04-16 14:47:19 +0200

    zellij plugins

--- modules/programs/zellij.nix
+++ modules/programs/zellij.nix
@@ -9,6 +9,20 @@
       on_force_close = "detach";
       pane_frames = false;
       default_layout = "compact";
+      # UGLY, but whatever
+      # plugins = {
+      #   "tab-bar location=\"zellij:tab-bar\"" = [ ];
+      #   "status-bar location=\"zellij:status-bar\"" = [ ];
+      #   "strider location=\"zellij:strider\"" = [ ];
+      #   "compact-bar location=\"zellij:compact-bar\"" = [ ];
+      #   "session-manager location=\"zellij:session-manager\"" = [ ];
+      #   "weclome-screen location=\"zellij:welcome-screen\"" = {
+      #     welcome_screen = true;
+      #   };
+      #   "filepicker location=\"zellij:strider\"" = {
+      #     cwd = "/";
+      #   };
+      # };
     };
   };
 }
