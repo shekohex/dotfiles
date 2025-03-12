eeceefadb70568bd833a4601a5e32408ccaa2f0b
Shady Khalifa <dev+github@shadykhalifa.me>
2025-03-12 20:57:42 +0200

    feat: add windsurf server

--- modules/services/vscode-server.nix
+++ modules/services/vscode-server.nix
@@ -2,11 +2,12 @@
 let
   isDarwin = pkgs.stdenv.isDarwin;
   isInsiders = false;
+  isWindsurf = true;
 in
 {
   services.vscode-server = {
     enable = !isDarwin;
     nodejsPackage = pkgs.nodejs_22;
-    installPath = if isInsiders then "$HOME/.vscode-server-insiders" else "$HOME/.vscode-server";
+    installPath = if isInsiders then "$HOME/.vscode-server-insiders" else if isWindsurf then "$HOME/.windsurf-server" else "$HOME/.vscode-server";
   };
 }
