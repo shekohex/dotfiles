9720d04c4af4b6211627dfdcba9bab9295515f25
Shady Khalifa <dev+github@shadykhalifa.me>
2025-08-13 22:36:58 +0300

    chore(services): disable cloudflared tunnel configuration

--- modules/services/cloudflared.nix
+++ modules/services/cloudflared.nix
@@ -1,13 +1,13 @@
 { user, ... }:
 {
-  services.cloudflared = {
-    enable = true;
-    tunnels = {
-      # workstation
-      "a922f05b-61d5-4c3f-8c91-425bbff52c49" = {
-        credentialsFile = "/home/${user}/.cloudflared/a922f05b-61d5-4c3f-8c91-425bbff52c49.json";
-        default = "http_status:404";
-      };
-    };
-  };
+  # services.cloudflared = {
+  #   enable = true;
+  #   tunnels = {
+  #     # workstation
+  #     "a922f05b-61d5-4c3f-8c91-425bbff52c49" = {
+  #       credentialsFile = "/home/${user}/.cloudflared/a922f05b-61d5-4c3f-8c91-425bbff52c49.json";
+  #       default = "http_status:404";
+  #     };
+  #   };
+  # };
 }
